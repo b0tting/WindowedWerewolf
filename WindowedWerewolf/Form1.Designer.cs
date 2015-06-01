650d5bf1ac9cbc077cbd046b7b54b1ea7a7b68e4
b0tting <otster@hotmail.com>
2015-06-02 00:09:42 +0200

    First commit

--- WindowedWerewolf/Form1.Designer.cs
+++ WindowedWerewolf/Form1.Designer.cs
@@ -28,32 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.playerList = new System.Windows.Forms.CheckedListBox();
+            this.addPlayer = new System.Windows.Forms.Button();
+            this.playerName = new System.Windows.Forms.TextBox();
+            this.deleteSelectedPlayer = new System.Windows.Forms.Button();
+            this.deleteSelectedRoles = new System.Windows.Forms.Button();
+            this.roleName = new System.Windows.Forms.TextBox();
+            this.roleList = new System.Windows.Forms.CheckedListBox();
+            this.roleAmount = new System.Windows.Forms.ListBox();
+            this.addRole = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // checkedListBox1
+            // playerList
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(13, 13);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(118, 169);
-            this.checkedListBox1.TabIndex = 0;
+            this.playerList.FormattingEnabled = true;
+            this.playerList.Location = new System.Drawing.Point(13, 13);
+            this.playerList.Name = "playerList";
+            this.playerList.Size = new System.Drawing.Size(188, 169);
+            this.playerList.TabIndex = 0;
+            this.playerList.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // addPlayer
+            // 
+            this.addPlayer.Location = new System.Drawing.Point(13, 302);
+            this.addPlayer.Name = "addPlayer";
+            this.addPlayer.Size = new System.Drawing.Size(118, 23);
+            this.addPlayer.TabIndex = 1;
+            this.addPlayer.Text = "Voeg speler toe";
+            this.addPlayer.UseVisualStyleBackColor = true;
+            this.addPlayer.Click += new System.EventHandler(this.addPlayer_Click);
+            // 
+            // playerName
+            // 
+            this.playerName.Location = new System.Drawing.Point(13, 276);
+            this.playerName.Name = "playerName";
+            this.playerName.Size = new System.Drawing.Size(118, 20);
+            this.playerName.TabIndex = 2;
+            // 
+            // deleteSelectedPlayer
+            // 
+            this.deleteSelectedPlayer.BackColor = System.Drawing.SystemColors.Control;
+            this.deleteSelectedPlayer.Location = new System.Drawing.Point(13, 189);
+            this.deleteSelectedPlayer.Name = "deleteSelectedPlayer";
+            this.deleteSelectedPlayer.Size = new System.Drawing.Size(188, 23);
+            this.deleteSelectedPlayer.TabIndex = 3;
+            this.deleteSelectedPlayer.Text = "Verwijder";
+            this.deleteSelectedPlayer.UseVisualStyleBackColor = false;
+            this.deleteSelectedPlayer.Click += new System.EventHandler(this.deleteSelectedPlayer_Click);
+            // 
+            // deleteSelectedRoles
+            // 
+            this.deleteSelectedRoles.Location = new System.Drawing.Point(207, 189);
+            this.deleteSelectedRoles.Name = "deleteSelectedRoles";
+            this.deleteSelectedRoles.Size = new System.Drawing.Size(185, 23);
+            this.deleteSelectedRoles.TabIndex = 6;
+            this.deleteSelectedRoles.Text = "Verwijder";
+            this.deleteSelectedRoles.UseVisualStyleBackColor = true;
+            this.deleteSelectedRoles.Click += new System.EventHandler(this.deleteSelectedRoles_Click);
+            // 
+            // roleName
+            // 
+            this.roleName.Location = new System.Drawing.Point(207, 276);
+            this.roleName.Name = "roleName";
+            this.roleName.Size = new System.Drawing.Size(118, 20);
+            this.roleName.TabIndex = 5;
+            // 
+            // roleList
+            // 
+            this.roleList.FormattingEnabled = true;
+            this.roleList.Location = new System.Drawing.Point(207, 13);
+            this.roleList.Name = "roleList";
+            this.roleList.Size = new System.Drawing.Size(185, 169);
+            this.roleList.TabIndex = 4;
+            // 
+            // roleAmount
+            // 
+            this.roleAmount.FormattingEnabled = true;
+            this.roleAmount.Location = new System.Drawing.Point(340, 276);
+            this.roleAmount.Name = "roleAmount";
+            this.roleAmount.Size = new System.Drawing.Size(52, 69);
+            this.roleAmount.TabIndex = 7;
+            // 
+            // addRole
+            // 
+            this.addRole.Location = new System.Drawing.Point(207, 302);
+            this.addRole.Name = "addRole";
+            this.addRole.Size = new System.Drawing.Size(118, 23);
+            this.addRole.TabIndex = 8;
+            this.addRole.Text = "Voeg rol toe";
+            this.addRole.UseVisualStyleBackColor = true;
+            this.addRole.Click += new System.EventHandler(this.addRole_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Speler ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Rol";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Aantal";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(133, 386);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(117, 54);
+            this.start.TabIndex = 12;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.checkedListBox1);
+            this.ClientSize = new System.Drawing.Size(1124, 720);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addRole);
+            this.Controls.Add(this.roleAmount);
+            this.Controls.Add(this.deleteSelectedRoles);
+            this.Controls.Add(this.roleName);
+            this.Controls.Add(this.roleList);
+            this.Controls.Add(this.deleteSelectedPlayer);
+            this.Controls.Add(this.playerName);
+            this.Controls.Add(this.addPlayer);
+            this.Controls.Add(this.playerList);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox playerList;
+        private System.Windows.Forms.Button addPlayer;
+        private System.Windows.Forms.TextBox playerName;
+        private System.Windows.Forms.Button deleteSelectedPlayer;
+        private System.Windows.Forms.Button deleteSelectedRoles;
+        private System.Windows.Forms.TextBox roleName;
+        private System.Windows.Forms.CheckedListBox roleList;
+        private System.Windows.Forms.ListBox roleAmount;
+        private System.Windows.Forms.Button addRole;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button start;
     }
 }
 
--- WindowedWerewolf/Form1.cs
+++ WindowedWerewolf/Form1.cs
@@ -15,6 +15,164 @@ namespace WindowedWerewolf
         public Form1()
         {
             InitializeComponent();
+            roleAmount.Items.AddRange(new String[] { PlayerRoles.ROLE_ANY_LEFT_LABEL, "1", "2", "3", "4", "5"});
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addPlayer_Click(object sender, EventArgs e)
+        {
+            String name = playerName.Text.Trim();
+            if (name.Length > 0 && !playerList.Items.Contains(name))
+            {
+                playerName.Text = "";
+                playerList.Items.Add(name);
+            }
+        }
+
+        private void addRole_Click(object sender, EventArgs e)
+        {
+            String role = roleName.Text.Trim();
+            if (role.Length > 0 && roleAmount.SelectedIndex >= 0)
+            {
+                PlayerRoles newPr = new PlayerRoles(role, roleAmount.SelectedItem.ToString());
+
+                // Vervang rol als hij al bestaat door de nieuwe suggestie
+                PlayerRoles removePr = null;
+                foreach (PlayerRoles pr in roleList.Items)
+                {
+                    if (pr.name.Equals(role))
+                    {
+                        removePr = pr;
+                    }
+                }
+                if (removePr != null)
+                {
+                    roleList.Items.Remove(removePr);
+                }
+
+                // En als we toch itereren, check dat er niet een tweede wildcard wordt toegevoegd. 
+                bool alreadyGotWildcard = false;
+                foreach (PlayerRoles pr in roleList.Items)
+                {
+                    if (pr.isWildCardRole())
+                    {
+                        alreadyGotWildcard = true;
+                    }
+                }
+                if (alreadyGotWildcard && newPr.isWildCardRole())
+                {
+                    alertBox("Er mag maar één rol zijn met 'overige' aantal");
+                }
+                else
+                {
+                    roleList.Items.Add(newPr);
+                }
+            }
+        }
+
+        private void deleteSelectedItems(CheckedListBox clb)
+        {
+            while (clb.CheckedItems.Count > 0)
+            {
+                clb.Items.RemoveAt(clb.CheckedIndices[0]);
+            }               
+        
+        }
+
+        private void deleteSelectedPlayer_Click(object sender, EventArgs e)
+        {
+            deleteSelectedItems(playerList);
+        }
+
+        private void deleteSelectedRoles_Click(object sender, EventArgs e)
+        {
+            deleteSelectedItems(roleList);
+        }
+
+        private void alertBox(String message)
+        {
+            MessageBox.Show(message, "Start spel", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Eerst controleren of er spelers zijn
+                int playerCount = playerList.Items.Count;
+                if (playerCount < 1)
+                {
+                    throw new Exception("Niet genoeg spelers voor een nieuw spel");
+                }
+
+                // Dan controleren of er wel rollen zijn, iets moeilijker..
+                bool wildCardRole = false;
+                int roleCount = 0;
+                for (int i = 0; i < roleList.Items.Count; i++)
+                {
+                    PlayerRoles pr = (PlayerRoles)roleList.Items[i];
+                    if (pr.isWildCardRole())
+                    {
+                        wildCardRole = true;
+                    }
+                    else
+                    {
+                        roleCount += pr.amount;
+                    }
+                }
+                if (roleCount < 1 && !wildCardRole)
+                {
+                    throw new Exception("Geen rollen gedefiniëerd");
+                }
+
+                // EVERY DAY I'M SHUFFLING!
+
+            }
+            catch (Exception exc)
+            {
+                alertBox(exc.Message);
+            }
+        }
+    }
+
+    class PlayerRoles
+    {
+        public static String ROLE_ANY_LEFT_LABEL = "(overige spelers)";
+        public static int ROLE_ANY_LEFT_AMOUNT = -1;
+
+        public String name;
+        public int amount;
+        public PlayerRoles(String role, String amount)
+        {
+            this.name = role;
+            if (ROLE_ANY_LEFT_LABEL.Equals(amount))
+            {
+                this.amount = ROLE_ANY_LEFT_AMOUNT;
+            }
+            else
+            {
+                this.amount = int.Parse(amount);
+            }
+        }
+
+        public bool isWildCardRole() {
+            return this.amount == ROLE_ANY_LEFT_AMOUNT;
+        }
+
+        public override string ToString() {
+            if (isWildCardRole())
+            {
+                return this.name + " " + ROLE_ANY_LEFT_LABEL;
+            }
+            else
+            {
+                return this.name + " (" + this.amount + ")";
+            }
+            
         }
     }
 }
